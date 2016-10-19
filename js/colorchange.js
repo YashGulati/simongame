46fed623869545c2fd60c87693a948e9dd375ee5
yash <yashgulati.g1@gmail.com>
2016-10-19 16:55:54 +0530

    color not changing

--- js/colorchange.js
+++ js/colorchange.js
@@ -1,7 +1,7 @@
 // js
 $(document).ready(function(){
-  $('#green').mousedown(function(){
-    $('#green').css('background-color','green');
+  $('#gameboard').mousedown(function(){
+    alert('a');
   });
   $('#green').mouseup(function(){
     $('#green').css('background-color','#00994D');
