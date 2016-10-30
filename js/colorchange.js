3471499c5a1d9b293d455dad94dd881ee47ea2b0
yash <yashgulati.g1@gmail.com>
2016-10-30 14:34:31 +0530

     

--- js/colorchange.js
+++ js/colorchange.js
@@ -1,9 +1,31 @@
 // js
 $(document).ready(function(){
   $('#green').mousedown(function(){
-    alert('a');
+    $('#green').css('background-color','green');
   });
   $('#green').mouseup(function(){
     $('#green').css('background-color','#00994D');
   });
+
+  $('#red').mousedown(function(){
+    $('#red').css('background-color','#ff4c4c');
+  });
+  $('#red').mouseup(function(){
+    $('#red').css('background-color','#9F0F17');
+  });
+
+  $('#blue').mousedown(function(){
+    $('#blue').css('background-color','blue');
+  });
+  $('#blue').mouseup(function(){
+    $('#blue').css('background-color','#004D99');
+  });
+
+  $('#yellow').mousedown(function(){
+    $('#yellow').css('background-color','#ffff5F');
+  });
+  $('#yellow').mouseup(function(){
+    $('#yellow').css('background-color','#FFFF00');
+  });
+
 });
